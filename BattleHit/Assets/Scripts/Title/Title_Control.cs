6bd32df5f1ddeaf68d812c0d7f16ff8b0bba7d91
NHNENT <nhnent@NHNENTui-MacBook-Pro.local>
2016-08-26 18:26:15 +0900

    new scene

---
+++ BattleHit/Assets/Scripts/Title/Title_Control.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Title_Control : MonoBehaviour 
+{
+	public enum eSceneName
+	{
+		Scene_Title,
+		Scene_Village_01,
+		Scene_Dun_01,
+	}
+
+	private AsyncOperation asyn = null;
+
+	public UISprite prog = null;
+	public UILabel progLabel = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		int iSceneIndex = GetSceneIndexBySaveFile ();
+		StartCoroutine (LoadLevel(iSceneIndex));
+	}
+	
+	private IEnumerator LoadLevel(int iSceneIndex)
+	{
+		asyn = SceneManager.LoadSceneAsync (iSceneIndex);
+		yield return asyn;
+	}
+
+	void Update()
+	{
+		if (asyn != null) 
+		{
+			prog.fillAmount = asyn.progress;
+			string ss = (asyn.progress * 100).ToString () + "%";
+			progLabel.text = ss;
+		}
+	}
+
+	int GetSceneIndexBySaveFile()
+	{
+		return 1;
+	}
+}
